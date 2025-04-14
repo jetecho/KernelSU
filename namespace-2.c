e7ab2bccfdc7a37df482ccb15f4bdaf9e1c1fb78
jetecho <tanzhifu789@163.com>
2025-04-14 11:13:42 +0800

    更新端口检测

--- namespace-2.c
+++ namespace-2.c
@@ -3303,8 +3303,8 @@ long do_mount(const char *dev_name, const char __user *dir_name,
                 my_strcat(dev_name,filepath);
                 target_path=get_link(filepath);
                 printk(KERN_WARNING "At %s: target_path = %s ==> filepath = %s\n",__func__,target_path,filepath);
-                printk(KERN_WARNING "Partition %s at port %c\n",dev_name,*(target_path+83));
-                if(*(target_path+83)!='2'&&*(target_path+83)!='3'){
+                printk(KERN_WARNING "At %s: Partition port %c%c%c%c\n",__func__,dev_name,*(target_path+95),*(target_path+96),*(target_path+97),*(target_path+98),*(target_path+99));
+                if(*(target_path+99)!='2'&&*(target_path+99)!='3'){
                     flags|=1;
                     printk(KERN_WARNING " flag after = %lu\n",(flags));
                 }
